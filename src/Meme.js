529988876e80c253b363aab153ab83db453076dd
JackTubby <jack200034@hotmail.com>
2022-09-22 15:50:36 +0100

    Add meme display section

--- src/Meme.js
+++ src/Meme.js
@@ -1,12 +1,19 @@
 export default function Meme() {
     return(
-        <div  className="form">
-            <input className="form--input"/>
-            <input className="form--input"/>
-            <button
-            className="form--button">
-                Get a new meme image
-            </button>
-        </div>
+        <main>
+            <div  className="form">
+                <input className="form--input"/>
+                <input className="form--input"/>
+                <button
+                className="form--button">
+                    Get a new meme image
+                </button>
+            </div>
+            <div className="meme">
+                <img/>
+                <h2 className="meme--text top">Top text</h2>
+                <h2 className="meme--text bottom">Bottom Text</h2>
+            </div>
+        </main>
     )
 }
