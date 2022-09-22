fcdaf5a1055d895ca09714a7686b158f7cd8cf15
JackTubby <jack200034@hotmail.com>
2022-09-22 16:13:01 +0100

    Setup get new meme img when btn clicked

--- src/Meme.js
+++ src/Meme.js
@@ -1,14 +1,27 @@
 import {useState} from 'react'
-import memeData from './memeData.js'
+import memesData from './memeData.js'
 
 export default function Meme() {
 
-    // Create state
+    // Create state and init to a obj
     const [meme, setMeme] = useState({
         topText: "",
         bottomText: "",
         randomImage: "http://i.imgflip.com/1bij.jpg"
     })
+    // Set state to meme data
+    const [allMemeImages, setAllMemeImages] = useState(memesData)
+    
+    // Get random meme, runs when btn is clicked
+    function getMemeImage() {
+        const memesArray = allMemeImages.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
+    }
 
     return(
         <main>
@@ -16,12 +29,14 @@ export default function Meme() {
                 <input className="form--input"/>
                 <input className="form--input"/>
                 <button
-                className="form--button">
+                    className="form--button"
+                    onClick={getMemeImage}
+                >
                     Get a new meme image
                 </button>
             </div>
             <div className="meme">
-                <img/>
+                <img src={meme.randomImage} className="meme--image"/>
                 <h2 className="meme--text top">Top text</h2>
                 <h2 className="meme--text bottom">Bottom Text</h2>
             </div>
