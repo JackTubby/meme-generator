4e9f97dfade5ac63fe0adaa0787770d5288c6aaf
JackTubby <jack200034@hotmail.com>
2022-09-22 12:50:57 +0100

    Create header component

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+export default function() {
+    return (
+        <header>
+            <img
+                src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FTrollface&psig=AOvVaw37DILLYWyq3SrnGQNMIp5x&ust=1663933794763000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCNDo38iqqPoCFQAAAAAdAAAAABAD"
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+        </header>
+    )
+}
