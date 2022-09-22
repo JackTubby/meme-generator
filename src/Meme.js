4111f37ee35c9f7ec6a946840081c7d2e9013027
JackTubby <jack200034@hotmail.com>
2022-09-22 15:36:06 +0100

    Create meme component

---
+++ src/Meme.js
@@ -0,0 +1,12 @@
+export default function Meme() {
+    return(
+        <div  className="form">
+            <input className="form--input"/>
+            <input className="form--input"/>
+            <button
+            className="form--button">
+                Get a new meme image
+            </button>
+        </div>
+    )
+}
