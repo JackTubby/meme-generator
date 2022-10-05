4a7edc72bef1b595a46ae4d2b3784c50c3802d3d
JackTubby <jack200034@hotmail.com>
2022-10-05 15:00:28 +0100

    Add comments

--- src/Meme.js
+++ src/Meme.js
@@ -8,20 +8,24 @@ export default function Meme() {
         bottomText: "",
         randomImage: "http://i.imgflip.com/1bij.jpg"
     })
-    // Set state to meme data
+    // Set state to meme data (pulled from API) | Init as an empty array
     const [allMemes, setAllMemes] = useState([])
 
     // Create API call for meme data
     useEffect(() => {
         fetch("https://api.imgflip.com/get_memes")
             .then(res => res.json())
+            // setAllMemes state to meme data
             .then(data => setAllMemes(data.data.memes))
     }, [])
     
     // Get random meme, runs when btn is clicked
     function getMemeImage() {
+        // Get's random number (max number however long all memes array length is (api data))
         const randomNumber = Math.floor(Math.random() * allMemes.length)
+        // Uses the random number and get's a url from the allMemes array
         const url = allMemes[randomNumber].url
+        // Spread in previous meme data and add new random image then set to the meme state
         setMeme(prevMeme => ({
             ...prevMeme,
             randomImage: url
@@ -30,8 +34,10 @@ export default function Meme() {
 
     // Updates text on meme image
     function handleChange(event) {
+        // Get name and value (need name to know whether it is top or btm txt)
         const {name, value} = event.target
         setMeme(prevMeme => ({
+            // Spread in the prev meme data but change the value to whatever is added to input
             ...prevMeme,
             [name]: value
         }))
